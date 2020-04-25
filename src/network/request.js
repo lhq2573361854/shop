341c0e56af332c9f73c6d37d63264dbf5847d6af
lhq2573361854 <48271487+lhq2573361854@users.noreply.github.com>
2020-04-25 23:19:12 +0800

    修改IP地址

--- src/network/request.js
+++ src/network/request.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export function request(config){
   const instance = axios.create({
-    baseURL:'http://123.207.32.32:8000/api/h8',
+    baseURL:'localhost:8080/api/h8',
     timeout:600000
   })
   instance.interceptors.request.use(config =>{
